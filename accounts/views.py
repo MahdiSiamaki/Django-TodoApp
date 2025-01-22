d521777b6693fa1a89e976dbb1b92df1e1a39625
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-22 14:16:52 +0330

    Add user login and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    fields = "username","password"
+    success_url = reverse_lazy('todo:todo_list')
+    
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super().form_valid(form)
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('accounts:login')
+    
+    def get_next_page(self):
+        next_page = super().get_next_page()
+        return next_page or self.next_page
+
+class UserSignupView(CreateView):
+    template_name = 'accounts/signup.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accounts:login')
+    
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = authenticate(
+            username=form.cleaned_data['username'],
+            password=form.cleaned_data['password1']
+        )
+        login(self.request, user)
+        return response
--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,6 @@ import debug_toolbar
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
+    path('', include('todo.urls')),
+    path('accounts/', include('accounts.urls')),
 ] 
