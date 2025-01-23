447b9e189c9bdeafe08390db0fd4d00d333b3a79
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-23 12:31:28 +0330

    Add tests to accounts and todo app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class UserAuthenticationTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.login_url = reverse('accounts:login')
+        self.logout_url = reverse('accounts:logout')
+        self.signup_url = reverse('accounts:signup')
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+    def test_login_view(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'testpassword'})
+        self.assertRedirects(response, reverse('todo:todo_list'))
+
+    def test_logout_view(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(self.logout_url)
+        self.assertRedirects(response, self.login_url)
+
+    def test_signup_url(self):
+        response = self.client.get(self.signup_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+
+
+    def test_invalid_login(self):
+        response = self.client.post(self.login_url, {'username': 'invaliduser', 'password': 'invalidpassword'})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+        self.assertContains(response, 'Invalid username or password.')
+
+    def test_invalid_signup(self):
+        response = self.client.post(self.signup_url, {
+            'username': 'newuser',
+            'password1': 'newpassword',
+            'password2': 'differentpassword'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/signup.html')
+        self.assertContains(response, 'The two password fields didn’t match.')
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,64 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Todo
 
-# Create your tests here.
+class TodoCRUDTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.login(username='testuser', password='testpassword')
+        self.todo = Todo.objects.create(user=self.user, title='Test Todo', description='Test Description', due_date='2023-12-31', done=False)
+
+    def test_todo_list_view(self):
+        response = self.client.get(reverse('todo:todo_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+        self.assertContains(response, self.todo.title)
+
+    def test_todo_detail_view(self):
+        response = self.client.get(reverse('todo:todo_detail', args=[self.todo.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_detail.html')
+        self.assertContains(response, self.todo.title)
+        self.assertContains(response, self.todo.description)
+
+    def test_todo_create_view(self):
+        response = self.client.get(reverse('todo:todo_create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_form.html')
+
+        response = self.client.post(reverse('todo:todo_create'), {
+            'title': 'New Todo',
+            'description': 'New Description',
+            'due_date': '2023-12-31',
+            'done': False
+        })
+        self.assertRedirects(response, reverse('todo:todo_list'))
+        self.assertTrue(Todo.objects.filter(title='New Todo').exists())
+
+    def test_todo_update_view(self):
+        response = self.client.get(reverse('todo:todo_update', args=[self.todo.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_form.html')
+
+        response = self.client.post(reverse('todo:todo_update', args=[self.todo.pk]), {
+            'title': 'Updated Todo',
+            'description': 'Updated Description',
+            'due_date': '2023-12-31',
+            'done': True
+        })
+        self.assertRedirects(response, reverse('todo:todo_list'))
+        self.todo.refresh_from_db()
+        self.assertEqual(self.todo.title, 'Updated Todo')
+        self.assertEqual(self.todo.description, 'Updated Description')
+        self.assertTrue(self.todo.done)
+
+    def test_todo_delete_view(self):
+        response = self.client.get(reverse('todo:todo_delete', args=[self.todo.pk]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_confirm_delete.html')
+
+        response = self.client.post(reverse('todo:todo_delete', args=[self.todo.pk]))
+        self.assertRedirects(response, reverse('todo:todo_list'))
+        self.assertFalse(Todo.objects.filter(pk=self.todo.pk).exists())
