aa46773d68bd7d38b03c247e7282c1a7e280c6c1
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-23 11:51:09 +0330

    Update admin pages

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+class CustomUserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
+    list_filter = ['is_staff']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    list_editable = ['is_staff']
+    list_per_page = 10
+    list_max_show_all = 100
+    ordering = ['username']
+    
+    actions = ['make_staff', 'make_not_staff']
+
+    def make_staff(self, request, queryset):
+        queryset.update(is_staff=True)
+    make_staff.short_description = 'Make selected users staff'
+
+    def make_not_staff(self, request, queryset):
+        queryset.update(is_staff=False)
+    make_not_staff.short_description = 'Make selected users not staff'
+
+# Unregister the default User admin
+admin.site.unregister(User)
+# Register User model with custom admin
+admin.site.register(User, CustomUserAdmin)
--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
 from  .models import Todo
 
-admin.site.register(Todo)
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'due_date', 'done']
+    list_filter = ['user', 'done']
+    search_fields = ['title', 'description']
+    list_editable = ['done']
+    list_per_page = 10
+    list_max_show_all = 100
+    date_hierarchy = 'due_date'
+    ordering = ['done', 'due_date']
+    readonly_fields = ['created_at', 'updated_at']
+    fieldsets = [
+        ('User', {'fields': ['user']}),
+        ('Task', {'fields': ['title', 'description', 'due_date', 'done']}),
+        ('Timestamps', {'fields': ['created_at', 'updated_at']}),
+    ]
+    add_fieldsets = [
+        ('User', {'fields': ['user']}),
+        ('Task', {'fields': ['title', 'description', 'due_date', 'done']}),
+    ]
+    actions = ['mark_as_done', 'mark_as_undone']
+
+    def mark_as_done(self, request, queryset):
+        queryset.update(done=True)
+
+    def mark_as_undone(self, request, queryset):
+        queryset.update(done=False)
+
+    mark_as_done.short_description = 'Mark selected todos as done'
+    mark_as_undone.short_description = 'Mark selected todos as undone'
