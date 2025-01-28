ba02b1932383b0247befebc06a2cdbe6403a3711
Mahdi Siamaki <siamaki.me@gmail.com>
2025-01-28 12:56:38 +0330

    Add-Swagger-And-Redoc-API-Documentation

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,23 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view=get_schema_view(
+    openapi.Info(
+        title="Todo API",
+        default_version='v1',
+        description="A simple Todo API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="siamaki.me@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +40,8 @@ urlpatterns = [
     path('', include('todo.urls')),
     path('accounts/', include('accounts.urls')),
     path('api-auth', include('rest_framework.urls')),
+    path('swagger/output.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ] 
