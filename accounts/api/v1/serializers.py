6d104f705989021070655a77b179185c0aecaccc
Mahdi Siamaki <siamaki.me@gmail.com>
2025-02-02 13:57:47 +0330

    Add validation for is_verified field in view and serializers

--- accounts/api/v1/serializers.py
+++ accounts/api/v1/serializers.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'is_verified']
